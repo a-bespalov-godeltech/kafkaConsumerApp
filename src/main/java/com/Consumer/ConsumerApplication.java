5fba7ef8edef3ff4170ab6871fcf099cde42f4dd
Aleksandr Bespalov <a.bespalov@godeltech.com>
2021-06-06 16:26:40 +0300

    Client added

--- src/main/java/com/Consumer/ConsumerApplication.java
+++ src/main/java/com/Consumer/ConsumerApplication.java
@@ -1,9 +1,14 @@
 package com.Consumer;
 
+import com.Consumer.config.KafkaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
+@EnableConfigurationProperties(KafkaConfig.class)
 public class ConsumerApplication {
 
   public static void main(String[] args) {
